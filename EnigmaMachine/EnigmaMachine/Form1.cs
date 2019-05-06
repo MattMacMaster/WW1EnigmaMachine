ee45c065fff128ab2ffc9de338fa9672aba8a9be
MortyAllen <matthmac@gmail.com>
2019-05-06 09:38:47 -0600

    General UI changes

--- EnigmaMachine/EnigmaMachine/Form1.cs
+++ EnigmaMachine/EnigmaMachine/Form1.cs
@@ -16,5 +16,15 @@ namespace EnigmaMachine
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
